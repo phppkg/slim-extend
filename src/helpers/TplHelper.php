e38af768c7d56855e473f713344c291debbc6167
inhere <in.798@qq.com>
2016-07-24 23:28:34 +0800

    some update

--- src/helpers/TplHelper.php
+++ src/helpers/TplHelper.php
@@ -4,9 +4,46 @@
  */
 namespace slimExt\helpers;
 
+use Slim;
 
 class TplHelper
 {
+    /**
+     * @example
+     *
+     * in view:
+     * {{ _globals.helper.csrfField() | raw }}
+     *
+     * @param Request $request
+     * @return string
+     */
+    public function csrfField()
+    {
+        /** @var Slim\Csrf\Guard */
+        $csrf = Slim::get('csrf');
+
+        // CSRF token name and value
+        $nameKey  = Slim::get('csrf')->getTokenNameKey();
+        $valueKey = Slim::get('csrf')->getTokenValueKey();
+
+        list($name,$value) = array_values(Slim::get('csrf')->generateToken());
+
+        return <<<EOF
+<input type="hidden" id="csrf_name" name="$nameKey" value="$name">
+<input type="hidden" id="csrf_value" name="$valueKey" value="$value">
+EOF;
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function csrfData()
+    {
+        return Slim::get('csrf')->generateToken();
+    }
+
+
     public function __call($method, array $args=[])
     {
         if ( function_exists($method) ) {
